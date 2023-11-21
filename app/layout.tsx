e1a1d32f8ce6e398d8d98fa225df08ce290ac957
Christie Pamphile <cpam2011@yahoo.com>
2023-11-21 17:49:34 -0500

    removed a slash

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ export const metadata: Metadata = {
   metadataBase: new URL(siteMetadata.siteUrl),
   title: {
     default: siteMetadata.title,
-    template: `%s | ${siteMetadata.title}`,
+    template: `%s  ${siteMetadata.title}`,
   },
   description: siteMetadata.description,
   openGraph: {
