c5c289f9575de3049c457b60ba4e1ef49e6878b2
Christie P <cpamp2011@gmail.com>
2024-06-15 03:33:58 -0400

    Update siteMetadata.js

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -4,7 +4,7 @@ const siteMetadata = {
   author: 'Christie Pamphile',
   headerTitle: '',
   description:
-    'Application Security Engineer with a passion for building beautiful, accessible, and performant web applications. Now securing the web one pixel at a time.',
+    'Security Engineer with a passion for building beautiful, accessible, and performant web applications. Now securing the web one pixel at a time.',
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://christiepamphile.com',
