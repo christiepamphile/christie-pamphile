59c3baaf40556d85467d4dee53900868cd8ebe36
Christie Pamphile <cpam2011@yahoo.com>
2024-06-18 00:07:59 -0400

    made changes within portfolio

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,5 +1,6 @@
 const headerNavLinks = [
   { href: '/', title: 'Home' },
+  { href: 'https://christiepdesign.netlify.app', title: 'UI Folio' },
   { href: '/projects', title: 'Projects' },
   { href: '/blog', title: 'Blog' },
   { href: '/about', title: 'About' },
